c8a434d73a36432a411d5468e379203968f38728
KaimNotark <kaimnotark@yandex.ru>
2019-11-12 12:00:43 +0800

    did buttons component

---
+++ src/components/CardOfApplicant.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CardOfApplicant",
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import '../stylesheets/variables.scss';
+
+</style>
