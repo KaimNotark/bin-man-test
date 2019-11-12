682b47989180e91fedd1f76df84c4c8554361801
KaimNotark <kaimnotark@yandex.ru>
2019-11-12 13:34:42 +0800

    add photos of applicant

---
+++ src/components/Applicant.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container">
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "Applicant"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../stylesheets/variables.scss";
+@import "../stylesheets/resets.scss";
+
+</style>
